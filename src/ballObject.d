8539f0b51c231fc417b22d958165466f975961b5
Kadeska <kadeska23@gmail.com>
2024-11-13 16:28:01 -0800

    update make.sh

--- src/ballObject.d
+++
@@ -1,5 +0,0 @@
-src/ballObject.o: src/ballObject.cpp src/../include/ballObject.hpp \
- include/glad/glad.h include/glad/khrplatform.h \
- src/../include/gameObject.hpp src/../include/glad/glad.h \
- src/../include/texture.hpp src/../include/spriteRenderer.hpp \
- src/../include/shader.hpp
